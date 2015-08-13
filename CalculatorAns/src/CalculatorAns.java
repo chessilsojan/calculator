82757d3d6bbdf6875dd5eb0b862aea95d965e859
jivinmathew <jivin4ever@gmail.com>
2015-08-13 17:34:32 +1200

    jivin commit

--- CalculatorAns/src/CalculatorAns.java
+++ CalculatorAns/src/CalculatorAns.java
@@ -1,13 +1,25 @@
 
-      if(op==2)
+            
+       if(op==3)
     {
         System.out.println("Enter your first num");
         num1=scan.nextDouble();
         System.out.println("Enter your second num");
         num2=scan.nextDouble();
-        ans = num1-num2;
+        ans = num1*num2;
         System.out.println(ans);
         
-    }      
+    }           
             
-     
+      if(op==4)
+    {
+        System.out.println("Enter your first num");
+        num1=scan.nextDouble();
+        System.out.println("Enter your second num");
+        num2=scan.nextDouble();
+        ans = num1/num2;
+        System.out.println(ans);
+        
+    }            
+  
+         

f038af81538501ff839c54b934ddecc319e67eba
ansmithjoseph <ansmithjosephnz@gmail.com>
2015-08-13 17:28:02 +1200

    ansmith1 commit

---
+++ CalculatorAns/src/CalculatorAns.java
@@ -0,0 +1,10 @@
+   if(op==1)
+    {
+        System.out.println("Enter your first num");
+        num1=scan.nextDouble();
+        System.out.println("Enter your second num");
+        num2 = scan.nextDouble();
+        ans  = num1 + num2;
+        System.out.println(ans);
+        
+    }
